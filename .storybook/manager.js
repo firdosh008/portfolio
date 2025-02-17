f0b4866f72ec0189d37cf440c9f6e2c1353f4786
ahmadfirdosh008 <ahmadfirdosh008@gmail.com>
2025-02-17 19:11:17 +0530

    update readme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.dark,
     brandImage: './icon.svg',
-    brandTitle: 'Firdohs ahmad Components',
+    brandTitle: 'Firdosh ahmad Components',
     brandUrl: 'https://hamishw.com',
   },
 });
