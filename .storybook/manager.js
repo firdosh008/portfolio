4f3078383743a17972bf6a59bea430efb6f0f781
ahmadfirdosh008 <ahmadfirdosh008@gmail.com>
2025-02-17 18:54:00 +0530

    name change

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.dark,
     brandImage: './icon.svg',
-    brandTitle: 'Hamish Williams Components',
+    brandTitle: 'Firdohs ahmad Components',
     brandUrl: 'https://hamishw.com',
   },
 });
